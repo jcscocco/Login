0dedbaaec8061e59504acdc2297057e4c6a13483
julio cesar da silva cocco <juliococco2801@gmail.com>
2026-04-08 19:56:34 -0300

    Thunder post login

--- Back_End/demo/demo/src/main/java/com/example/demo/LoginController.java
+++ Back_End/demo/demo/src/main/java/com/example/demo/LoginController.java
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin(origins = "*")
 public class LoginController {
 
     @PostMapping
     public String login(@RequestBody Usuario usuario) {
-        if(usuario.getEmail().equals("admin@email.com") && usuario.getSenha().equals("123")) {
+        if(usuario.getEmail().equals("juliococco2801@gmail.com") && usuario.getSenha().equals("12345        ")) {
             return "Login OK";
         } else {
             return "Login inválido";
--- Front_End/script.js
+++ Front_End/script.js
@@ -1,10 +1,24 @@
-function login() {
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("senha").value;
+async function login() {
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
 
-  if(email === "juliococco2801@gmail.com" && senha === "Juliocesar280108") {
-    alert("Login realizado com sucesso!");
-  } else {
-    alert("Email ou senha incorretos");
-  }
+    try {
+        const resposta = await fetch("http://localhost:8080/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                senha: senha
+            })
+        });
+
+        const resultado = await resposta.text();
+
+        alert(resultado);
+    } catch (erro) {
+        alert("Erro ao conectar com o servidor");
+        console.error(erro);
+    }
 }
