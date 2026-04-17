3af5f4a5869bd0b4261eda22f949bf655a1b0a60
julio cesar da silva cocco <juliococco2801@gmail.com>
2026-04-17 01:36:09 -0300

    Reajuste

--- Front_End_Login/Cadastro/script.js
+++ Front_End_Login/Cadastro/script.js
@@ -1,20 +1,16 @@
 function mostrarLogin() {
   document.getElementById('loginForm').classList.add('active');
   document.getElementById('cadastroForm').classList.remove('active');
-
   document.getElementById('btnLogin').classList.add('active');
   document.getElementById('btnCadastro').classList.remove('active');
-
   document.getElementById('toggle').classList.remove('signup-active');
 }
 
 function mostrarCadastro() {
   document.getElementById('cadastroForm').classList.add('active');
   document.getElementById('loginForm').classList.remove('active');
-
   document.getElementById('btnCadastro').classList.add('active');
   document.getElementById('btnLogin').classList.remove('active');
-
   document.getElementById('toggle').classList.add('signup-active');
 }
 
@@ -22,7 +18,7 @@ async function cadastro() {
   const email = document.getElementById("emailCadastro").value;
   const senha = document.getElementById("senhaCadastro").value;
 
-  const resposta = await fetch("http://localhost:8080/cadastro", {
+  const resposta = await fetch("http://localhost:8080/auth/cadastro", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, senha })
@@ -36,7 +32,7 @@ async function login() {
   const email = document.getElementById("emailLogin").value;
   const senha = document.getElementById("senhaLogin").value;
 
-  const resposta = await fetch("http://localhost:8080/login", {
+  const resposta = await fetch("http://localhost:8080/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, senha })
